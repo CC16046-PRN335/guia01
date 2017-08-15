ab2a94797880e03b891b177cb8cea467804363d9
FRANK M <FRANK M@escritorio>
2017-08-15 17:01:52 -0600

    Metodos getResume y Capitalizar creados sin errores

---
+++ guia01/src/main/java/NewClass.java
@@ -0,0 +1,24 @@
+
+import com.mycompany.guia01.Utilities;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author FRANK M
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        Utilities u = new Utilities();
+        String cadena="reree gfgfdf fdfhgfgh";
+        JOptionPane.showMessageDialog(null,u.capitalizar(cadena));
+        
+        
+        
+    }
+}
--- guia01/src/main/java/com/mycompany/guia01/Utilities.java
+++ guia01/src/main/java/com/mycompany/guia01/Utilities.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.guia01;
 
-/**
- *
- * @author FRANK M
- */
+
 public class Utilities {
+  String resume;
+  /* Este metodo se encarga de extraer los primeros 30 caracteres de una cadena de texto a traves de la variable llamada "cadena"
+  que se encarga de ir guardando cada caracter para despues pasarlo a la variable "resume" e imprimir los caracteres*/
+    public String getResume(String resume){
+        String cadena="";
+        for(int i=0;i<30;i++){
+            cadena=cadena+resume.charAt(i);
+        }
+        resume=cadena;
+        return resume;   
+    }
     
+    public String capitalizar(String resume){
+        String cadena="";
+        resume.replaceAll("  ", " ");
+        for(int i=1; i<resume.length();i++){
+            if(resume.charAt(i-1)==' ' || resume.charAt(i)==' ' ){
+                cadena += String.valueOf(resume.charAt(i)).toUpperCase();
+                
+            }else{
+                cadena += resume.charAt(i);
+            }
+        }
+        return cadena;
+    }
 }

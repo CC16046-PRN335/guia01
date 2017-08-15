1553f86ca345d5004e11113fc8ed6f419b7cc335
FRANK M <FRANK M@escritorio>
2017-08-15 17:11:18 -0600

    cambio de la variable resume por texto

--- guia01/src/main/java/com/mycompany/guia01/Utilities.java
+++ guia01/src/main/java/com/mycompany/guia01/Utilities.java
@@ -3,27 +3,27 @@ package com.mycompany.guia01;
 
 
 public class Utilities {
-  String resume;
+  String texto;
   /* Este metodo se encarga de extraer los primeros 30 caracteres de una cadena de texto a traves de la variable llamada "cadena"
   que se encarga de ir guardando cada caracter para despues pasarlo a la variable "resume" e imprimir los caracteres*/
-    public String getResume(String resume){
+    public String getResume(String texto){
         String cadena="";
         for(int i=0;i<30;i++){
-            cadena=cadena+resume.charAt(i);
+            cadena=cadena+texto.charAt(i);
         }
-        resume=cadena;
-        return resume;   
+        texto=cadena;
+        return texto;   
     }
     
-    public String capitalizar(String resume){
+    public String capitalizar(String texto){
         String cadena="";
-        resume.replaceAll("  ", " ");
-        for(int i=1; i<resume.length();i++){
-            if(resume.charAt(i-1)==' ' || resume.charAt(i)==' ' ){
-                cadena += String.valueOf(resume.charAt(i)).toUpperCase();
+        texto.replaceAll("  ", " ");
+        for(int i=1; i<texto.length();i++){
+            if(texto.charAt(i-1)==' ' || texto.charAt(i)==' ' ){
+                cadena += String.valueOf(texto.charAt(i)).toUpperCase();
                 
             }else{
-                cadena += resume.charAt(i);
+                cadena += texto.charAt(i);
             }
         }
         return cadena;
